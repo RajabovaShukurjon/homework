ece9b2a669c4df2a4e39b85b325e23132afac04d
RajabovaShukurjon <rajabovashukurjon16@gmail.com>
2025-11-22 04:14:32 +0500

    Create Math.moduli.homework.py

---
+++ Math.moduli.homework.py
@@ -0,0 +1,115 @@
+# ceil
+import math
+
+# x = 4.2
+# y = math.ceil(x)
+# print(y)
+
+# x = 5.4
+# y = math.ceil(x)
+# print(y)
+
+
+# fabs
+
+# x = -7.5
+# y = math.fabs(x)
+# print(y)
+
+# x = -10
+# y = math.fabs(x)
+# print(y)
+
+
+# # floor
+# x = 5.9
+# y = math.floor(x)
+# print(y)
+#
+# x = 10.2
+# y = math.floor(x)
+# print(y)
+
+
+# exp
+# x = 2
+# y = math.exp(x)
+# print(y)
+#
+# x = 4
+# y = math.exp(x)
+# print(y)
+
+# cos
+# y = math.cos(0)
+# print(y)
+#
+# x = math.cos(3.14/3)
+# print(x)
+
+
+# sin
+# y = math.sin(0)
+# print(y)
+#
+# x = math.sin(3.14/2)
+# print(x)
+
+
+# tan
+# y = math.tan(1)
+# print(y)
+#
+# x = math.tan(3.14/2)
+# print(x)
+
+# deg = 60
+# rad = math.radians(deg)
+# print(math.cos(rad))
+#
+#
+# deg = 30
+# rad = math.radians(deg)
+# print(math.cos(rad))
+
+
+
+
+# degrees(rad)
+# rad = math.pi / 3
+# deg = math.degrees(rad)
+# print(deg)
+#
+# rad = math.pi / 2
+# deg = math.degrees(rad)
+# print(deg)
+#
+#
+# rad = math.pi
+# deg = math.degrees(rad)
+# print(deg)
+
+# radians()
+# deg = 90
+# rad = math.radians(deg)
+# print(rad)
+#
+# deg = 180
+# rad = math.radians(deg)
+# print(rad)
+
+
+# e
+print(math.e)
+
+
+
+# round
+x = 12.1324
+y = round(x, 1)
+print(y)
+
+
+x = 35.56789
+y = round(x, 2)
+print(y)
